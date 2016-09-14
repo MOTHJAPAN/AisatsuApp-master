9089580e87905eecb0358826ee5d85910c33ca30
MOTHJAPAN <takafumi@kiloi.net>
2016-09-14 18:44:28 +0900

    再提出

---
+++ app/src/main/java/jp/techacademy/takafumi/asakura/aisatsuapp/MainActivity.java
@@ -0,0 +1,78 @@
+package jp.techacademy.takafumi.asakura.aisatsuapp;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button1) {
+            showTimePickerDialog() ;
+
+        }
+
+
+        }
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Log.d("UI-PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+
+                        //ここでIF等設定
+                        if(2 <= hourOfDay){
+                            mTextView.setText("おはよう");
+
+                        }
+                        else if (10 <= hourOfDay){
+                            mTextView.setText("こんにちは");
+
+                        }
+
+
+                        else if (18 <= hourOfDay){
+                            mTextView.setText("こんばんわ");
+
+                        }
+
+                    }
+                },
+
+
+
+                1, // 初期値（時間）
+                0,  // 初期値（分）
+                true);
+        timePickerDialog.show();
+    }
+
+}
+
+
+
+
+
+
